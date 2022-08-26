2d75929351670e2e7370d6d819acc17af063cac7
GribovAlexey <gribov.alexey.m@yandex.ru>
2022-08-26 15:04:44 +0300

    Some basic

--- middleware_test/settings.py
+++ middleware_test/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'middleware_test.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'mwt',
+       'USER': 'mwt_admin',
+       'PASSWORD': 'mwt_admin',
+       'HOST': '127.0.0.1',
+       'PORT': '',
+   }
 }
 
 
